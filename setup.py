6075ad24a1e87548d1e7ba1773614a4a3e317aa7
charleszxiong <xiongzhaoxi@gmail.com>
2019-11-04 21:59:33 -0800

    restructure

--- setup.py
+++ setup.py
@@ -3,22 +3,19 @@ from distutils.core import setup
 with open("README", 'r') as f:
     long_description = f.read()
 
-setup(name='rmbs',
-      version='0.1.0',
-      url='https://github.com/charleszxiong/rmbs',
-      description='Reconstruct Quantum Many-Body States from Correlation Functions',
-      long_description=long_description,
-      author='Charles Zhaoxi Xiong',
-      author_email='xiongzhaoxi@gmail.com',
-      license='MIT',
-      packages=['tensornet',
-                'reconstruct'
-                ],
-      requires=['numpy',
-                'scipy',
-                'pandas'
-                ],
-      package_data={'tensornet': ['data/*'],
-                    'reconstruct': ['data/*']
-                    }
-      )
+setup(
+    name='rmbs',
+    version='0.1.0',
+    url='https://github.com/charleszxiong/rmbs',
+    description='Reconstruct Quantum Many-Body States from Correlation Functions',
+    long_description=long_description,
+    author='Charles Zhaoxi Xiong',
+    author_email='xiongzhaoxi@gmail.com',
+    license='MIT',
+    packages=['rmbs', 'rmbs.models'],
+    requires=[
+        'numpy >= 1.16.3',
+        'scipy >= 1.3.0',
+        'pandas >= 0.24.2'
+    ]
+)
